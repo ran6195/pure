54344472ee2e4feea15261b56a7b0bc81a4a60ee
Corsanello Francesco <37504667@telecomitalia.local>
2017-10-10 14:47:39 +0200

    Connessione OK

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 let Boxes = [{
     nome: "PUREBO01" ,
     indirizzo: "10.38.110.160" ,
-    token: "585e8ba7-ed6e-d6d7-35b0-51d7c77ecf16"
+    token: "585e8ba7-ed6e-d6d7-35b0-51d7c77ecf16" ,
+    api_version : "1.8"
   }];
   
   
--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+//Carico il file di configurazione che contiene un array con le configurazioni dei vari box
 const boxes = require( './config.js' )
 
-console.log( boxes );
+//Carico le librerie dei pure
+const PureArray = require( './node_modules/node-purestorage/lib/PureArray.js' )
+
+//carico la libreria delle sessioni
+const con = require( './node_modules/node-purestorage/lib/session.js' )
+
+//effettuoa la connessione ad uno storage
+
+let C = new con( boxes[ 0 ].indirizzo , boxes[ 0 ].api_version )
+
+//Creo l'oggetto  (Promise) relatico alla connessione
+let B = new PureArray( C );
+
+
+//mi connetto al box
+
+C.authSession( boxes[ 0 ].token ).then(() => console.log('connessione ok') , ( err ) => console.log( err ) )
+
+
+
+
 
